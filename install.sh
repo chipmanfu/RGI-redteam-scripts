ace5c60f544a0b0ce2e0cdc9fd23e146522e798d
chipmanfu <50666533+chipmanfu@users.noreply.github.com>
2022-08-02 19:39:02 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-cd RGI-redteam-scripts
 mv backbonerouters /root/
 mv scripts /root/
 chmod 755 /root/scripts/*.sh
 chmod 755 /root/scripts/phish/*.sh
 mv /root/backbonerouters/hosts /etc/hosts
-cd ..
+cd /root/
 rm -r RGI-redteam-scripts
